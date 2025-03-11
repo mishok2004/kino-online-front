b5923128ae3f6465b1d3617b8c775fd13e04b646
mishok2004 <144279964+mishok2004@users.noreply.github.com>
2025-03-11 16:44:00 +0300

    add layout

---
+++ app/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+import styles from './Navigation.module.scss'
+
+const Navigation: FC = () => {
+	return <div></div>
+}
+export default Navigation
---
+++ app/components/screen/home/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { IHome } from './home.interface'
+
+const Home: FC<IHome> = () => {
+	return (
+		<div>
+			<h1>hp</h1>
+		</div>
+	)
+}
+
+export default Home
