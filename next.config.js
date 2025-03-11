571a81b2804be5c52915fcd7f5f1ac5addb08c50
mishok2004 <144279964+mishok2004@users.noreply.github.com>
2025-03-11 12:43:09 +0300

    config prettier, add alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+    APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
+  }
 }
 
 module.exports = nextConfig
